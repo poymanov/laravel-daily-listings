33b3d7cd295054b331e3a53691d5faf3e1c1d1a8
poymanov <n.poymanov@gmail.com>
2021-09-19 17:14:55 +0300

    Добавлена документация

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -14,6 +14,18 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Message newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Message query()
  * @mixin \Eloquent
+ * @property int $id
+ * @property int $user_id
+ * @property int $listing_id
+ * @property string $text
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereListingId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereText($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Message whereUserId($value)
  */
 class Message extends Model
 {
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+# Документация
+
+* [Введение](01-intro/README.md)
+* [Установка](02-setup/README.md)
+* [Запуск](03-start/README.md)
+* [Регистрация](04-registration/README.md)
+* [Аутентификация](05-auth/README.md)
+* [Просмотр предложений](06-listings/README.md)
+* [Создание предложения](07-create-listing/README.md)
+* [Редактирование предложения](08-edit-listing/README.md)
+* [Удаление предложения](09-delete-listing/README.md)
+* [Добавление предложения в избранное](10-favourite-listing/README.md)
+* [Отправка сообщения автору предложения](11-send-message/README.md)
